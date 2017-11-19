100272df6ff36aa62437d351db825da0b1601eeb
Mars Howard <marsbhoward@gmail.com>
2017-11-19 16:51:58 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,31 +7,31 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/posts/new' do #loads new form
+  get '/posts/new' do
       erb :new
     end
 
-    post '/posts' do  
+    post '/posts' do
       @post = Post.create(params)
       redirect to '/posts'
     end
 
-    get '/posts' do 
+    get '/posts' do
       @posts = Post.all
       erb :index
     end
 
-    get '/posts/:id' do 
+    get '/posts/:id' do
       @post = Post.find_by_id(params[:id])
       erb :show
     end
 
-    get '/posts/:id/edit' do 
+    get '/posts/:id/edit' do
       @post = Post.find_by_id(params[:id])
       erb :edit
     end
 
-    patch '/posts/:id' do  
+    patch '/posts/:id' do
       @post = Post.find_by_id(params[:id])
       @post.name = params[:name]
       @post.content = params[:content]
@@ -40,7 +40,7 @@ class ApplicationController < Sinatra::Base
     end
 
 
-    delete '/posts/:id/delete' do 
+    delete '/posts/:id/delete' do
       @post = Post.find_by_id(params[:id])
       @post.delete
       redirect to '/posts'
